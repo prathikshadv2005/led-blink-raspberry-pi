c4c7f8139838405e37290ea5aebea7b4fdd78c89
Prathiksha D V <prathikshadv2005@gmail.com>
2026-03-06 19:35:56 +0530

    Add LED blinking functionality using RPi.GPIO

---
+++ led_blink.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+LED_PIN = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(LED_PIN, GPIO.HIGH)  # LED ON
+        time.sleep(1)
+
+        GPIO.output(LED_PIN, GPIO.LOW)   # LED OFF
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
